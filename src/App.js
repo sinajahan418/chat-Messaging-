3d4dfedc54aa598edb92ec1594e9619ec4a923a9
sina jahan <sinajahan418@example.com>
2024-07-27 06:34:45 -0700

    saidebar and login

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import "./App.css";
 import { StreamChat } from "stream-chat";
 import { Chat } from "stream-chat-react";
-import Cookies from "universal-cookie"
+import Cookies from "universal-cookie";
 import ChanelListContainer from "./components/ChanelListContainer";
+import Auth from "./components/Auth";
 
-const apikey = "8v4jza8pna5t"
-const cleint = StreamChat.getInstance(apikey)
+const apikey = "8v4jza8pna5t";
+const cleint = StreamChat.getInstance(apikey);
 
 function App() {
-  return <div className="app__wrapper">
-
+  const auth = false;
+  if (!auth) {
+    return <Auth />;
+  }
+  return (
+    <div className="app__wrapper">
       <Chat client={cleint} theme="them light">
-          <ChanelListContainer>
-            
-          </ChanelListContainer>
+        <ChanelListContainer></ChanelListContainer>
       </Chat>
-  </div>;
+    </div>
+  );
 }
 
 export default App;
-
---
+++ src/components/TeamChannelList.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const TeamChannelList = ({
+  setToggleContainer,
+  children,
+  error = false,
+  loading,
+  type,
+  isCreating,
+  setIsCreating,
+  setCreateType,
+  setIsEditing,
+}) => {
+  if (error) {
+    return type === "team" ? (
+      <div className="team-channel-list">
+        <p className="team-channel-list__message">
+          Connection error, please wait a moment and try again.
+        </p>
+      </div>
+    ) : null;
+  }
+
+  if (loading) {
+    return (
+      <div className="team-channel-list">
+        <p className="team-channel-list__message loading">
+          {type === "team" ? "Channels" : "Messages"} loading...
+        </p>
+      </div>
+    );
+  }
+  return <div>lll</div>;
+};
+
+export default TeamChannelList;
