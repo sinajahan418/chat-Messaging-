e8e15f758e0cf954c83bc57fe4c71e12a26de2ef
sina jahan <sinajahan418@example.com>
2024-07-28 01:38:47 -0700

    end  of the chat projext

--- src/App.js
+++ src/App.js
@@ -5,17 +5,32 @@ import Cookies from "universal-cookie";
 import ChanelListContainer from "./components/ChanelListContainer";
 import Auth from "./components/Auth";
 
+const cookies = new Cookies();
 const apikey = "8v4jza8pna5t";
-const cleint = StreamChat.getInstance(apikey);
+const client = StreamChat.getInstance(apikey);
+const authToken = cookies.get("token");
+
+if (authToken) {
+  client.connectUser(
+    {
+      id: cookies.get("userId"),
+      name: cookies.get("username"),
+      fullName: cookies.get("fullName"),
+      image: cookies.get("avatarURL"),
+      hashedPassword: cookies.get("hashedPassword"),
+      phoneNumber: cookies.get("phoneNumber"),
+    },
+    authToken
+  );
+}
 
 function App() {
-  const auth = false;
-  if (!auth) {
+  if (!authToken) {
     return <Auth />;
   }
   return (
     <div className="app__wrapper">
-      <Chat client={cleint} theme="them light">
+      <Chat client={client} theme="them light">
         <ChanelListContainer></ChanelListContainer>
       </Chat>
     </div>
